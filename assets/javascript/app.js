a5d7c32532831fe54dac82caeb3bc3e9430c5765
jsalamat <salamat_jon@yahoo.com>
2017-11-08 02:08:12 -0500

    second hand fix and css animate work on second hands

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+const secondHand = document.querySelector('.second-hand');
+
 const setDate = () => {
 	const now = new Date();
 	const seconds = now.getSeconds();
@@ -6,6 +8,9 @@ const setDate = () => {
 	const secondsDegrees = ((seconds / 60) * 360);
 	/* this how we convert seconds to degrees to be applied on second hand */
 	console.log(secondsDegrees);
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	/* when we select the second-hand and apply styles to it */
+	/* rotate added here is usually 100 deg but instead select secondDegrees */
 }
 
 setInterval(setDate, 1000);
