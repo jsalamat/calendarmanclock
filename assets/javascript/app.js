273edcf2a3b8cf0ed431b72c944103331a56dd87
jsalamat <salamat_jon@yahoo.com>
2017-11-08 01:47:44 -0500

    convert seconds to degrees to be applied on second hand

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
 const setDate = () => {
 	const now = new Date();
 	const seconds = now.getSeconds();
+	/* this to get the date now with it we get the seconds now*/
 	console.log(seconds);
+	const secondsDegrees = ((seconds / 60) * 360);
+	/* this how we convert seconds to degrees to be applied on second hand */
+	console.log(secondsDegrees);
 }
 
 setInterval(setDate, 1000);
